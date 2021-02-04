b1bf0a434816637dd3e289b96d55fd71432e35c7
Ceroy <abhishek.k@technovert.com>
2021-02-04 15:18:41 +0530

    Comments written in JS file and Formatting of the CSS file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Contact class for each instance of the data
 class Contact {
     //Private Static id for Unique Identifier
     static id = 0;
@@ -12,6 +13,8 @@ class Contact {
     }
 }
 
+
+//Dummy data shown at the start of the page in the contacts array used throughout the page
 var contacts = new Array(
     new Contact('Chandermani Arora', 'chandermani@technovert.com', '+91 7845120215', '01234058490', 'https://www.technovert.com', "Madhuri Hills"),
     new Contact('Sashi Pagadala', 'vijay@technovert.com', '+91 9875841236', '01234058490', 'https://www.technovert.com', "Madhuri Hills"),
@@ -19,12 +22,14 @@ var contacts = new Array(
     new Contact('Vijay Yalamanchali', 'vijay@technovert.com', '+91 9292929292', '01234058490', 'https://www.technovert.com', "Madhuri Hills")
 );
 
-
+//Adds the data to the contacts array and calls the displayContactList() function
 function addContact(contact) {
     contacts.push(contact);
     displayContactList(contact);
 }
 
+
+//Edits the data, takes in Contact class and oldId as parameter. Changes the data by iterating the contacts array and changing the data in the DOM
 function editContact(newContact, oldId) {
 
     //Updates the contact in contacts array
@@ -57,6 +62,7 @@ function editContact(newContact, oldId) {
     selectedContact(NaN);
 }
 
+//Deletes the data by filtering the contacts array and removing the element from the DOM
 function deleteContact(id) {
     contacts = contacts.filter((contact) => {
         return contact.id != id;
@@ -69,6 +75,7 @@ function deleteContact(id) {
 }
 
 
+//Displays the left side contact list. If param is empty means new contact to be inserted, else iterates over the contacts array and creates the elements in the DOM
 function displayContactList(param) {
     const contactList = document.getElementsByClassName('contacts-list')[0];
     if (param == undefined) {
@@ -139,10 +146,18 @@ function displayContactList(param) {
     }
 }
 
+//Toggles the background of contact selected in the left side contacts list by taking id as parameter, if parameter NaN means no contact has background color else the 
+//specific id contact gets the background color by adding the 'selected' css class
 function selectedContact(id) {
+
+    //Getting the reference of the left side list
     const contactList = document.getElementsByClassName('contacts-list')[0];
+
+    //Spread to convert childrence of contactlist into array
     const contactListChild = [...contactList.children];
     contactListChild.forEach((contact) => {
+
+        //if match found, the selected class is added else removed
         if (contact.id == id) {
             contact.classList.add('selected');
         }
@@ -154,7 +169,12 @@ function selectedContact(id) {
 
 }
 
+
+//Displays the selected contact in the center main elements by changing the visibility and changing the values in the DOM. Takes the Contact class object
+//as parameter, sent each time 'click' event listener is triggered in the contact in the contact lists directly
 function displaySelectedContact(contact) {
+
+    //Getting the reference from the DOM
     const name = document.getElementById('selected-name');
     const email = document.getElementById('selected-email').lastChild;
     const mobile = document.getElementById('selected-mobile').lastChild;
@@ -162,8 +182,11 @@ function displaySelectedContact(contact) {
     const website = document.getElementById('selected-website').lastChild;
     const address = document.getElementById('selected-address').lastChild;
     const id = document.getElementById('selected-id');
+
+    //Removing the visibility of the form
     document.getElementById('form').style.display = "none";
 
+    //Getting the reference and displaying the contact in the center page by changing the values in the DOM
     const div = document.getElementById('contact-selected');
 
     div.style.display = "block";
@@ -176,6 +199,8 @@ function displaySelectedContact(contact) {
     id.value = contact.id;
 }
 
+//The form to gather or update the details. If 'id' is missing means empty form is called to add new contact data else update operation is called
+//and the form fields are populated based on the contact with the same id.
 function contactDetailsForm(id) {
 
     //Reference to all the input fields
@@ -188,7 +213,7 @@ function contactDetailsForm(id) {
     const formId = document.getElementById('form-id');
     const formSubmitBtn = document.getElementById('form-submit-btn');
 
-    //To preven submit event because of enter key
+    //To prevent submit event because of enter key
     formName.onkeypress = (e) => e.key!="Enter";
     formEmail.onkeypress = (e) => e.key!="Enter";
     formMobile.onkeypress = (e) => e.key!="Enter";
@@ -233,6 +258,7 @@ function contactDetailsForm(id) {
         }
     }
 
+    //Substituting the values
     formName.value = nameVal;
     formEmail.value = emailVal;
     formMobile.value = mobileVal;
@@ -246,13 +272,23 @@ function contactDetailsForm(id) {
 
 
 /* All Event Listeners Globals*/
+
+//As soon as page loads and the DOM is loaded the function is called so as to iterate over the 
 document.addEventListener('DOMContentLoaded', displayContactList());
+
+//Home button in the navigation when clicked
 var homeBtn = document.getElementById('home').addEventListener('click', (e) => {
+
+    //Remove the background color from all the contact lists
     selectedContact(NaN);
+
+    //Makes the form and the selected contact invisible
     document.getElementById('contact-selected').style.display = "none";
     document.getElementById('form').style.display = "none";
 });
 
+
+//When delete button is selected from the contacts display
 var selectedDeleteBtn = document.getElementById('selected-delete').addEventListener('click', (e) => {
     let id = document.getElementById('selected-id').value;
     deleteContact(id);
@@ -260,12 +296,14 @@ var selectedDeleteBtn = document.getElementById('selected-delete').addEventListe
     document.getElementById('form').style.display = "none";
 });
 
+//When add button is clicked from the navigation bar
 var addBtn = document.getElementById('add').addEventListener('click', (e) => {
     document.getElementById('contact-selected').style.display = "none";
     document.getElementById('form').style.display = "block";
     contactDetailsForm();
 });
 
+//When edit button is selected from the contacts display
 var selectedEditBtn = document.getElementById('selected-edit').addEventListener('click', (e) => {
     let id = document.getElementById('selected-id').value;
     contactDetailsForm(id);
@@ -273,6 +311,7 @@ var selectedEditBtn = document.getElementById('selected-edit').addEventListener(
     document.getElementById('form').style.display = "block";
 });
 
+//When cancel button is clicked within the form
 var cancelButtonForm = document.getElementById('form-cancel-btn').addEventListener('click',(e)=>{
     document.getElementById('home').click();
 });
@@ -291,6 +330,8 @@ var form = document.getElementById('form').addEventListener('submit', (e) => {
     let idVal = e.target[6].value;
 
     let newContact = new Contact(nameVal, emailVal, mobileVal, landlineVal, websiteVal, addressVal);
+
+    //Input type hidden contains the id. When id is none means new contact is added else existing contact is updated
     if (idVal == '') {
         addContact(newContact);
     }
@@ -303,9 +344,17 @@ var form = document.getElementById('form').addEventListener('submit', (e) => {
 
 
 /*Form Validation for Name */
+
+//Reference of the input field
 var inputName = document.getElementById('form-name');
+
+//Reference of the element that displays error
 var inputNameErrorMessage = document.getElementById('form-name-error');
+
+//Eventlistener Keyup to validate input on each key up
 inputName.addEventListener('keyup', (e) => {
+
+    //Regular Expression
     let regex = /^[a-zA-Z\s]+$/g;
     if (!regex.test(e.target.value)) {
         inputName.classList.add('form-error');
@@ -318,9 +367,17 @@ inputName.addEventListener('keyup', (e) => {
 });
 
 /*Form Validation for Emails */
+
+//Refernce to the input field
 var inputEmail = document.getElementById('form-email');
+
+//Reference to the element that shows the error
 var inputEmailErrorMessage = document.getElementById('form-email-error');
+
+//Eventlistener Keyup to validate input on each key up
 inputEmail.addEventListener('keyup', (e) => {
+
+    //Regular Expression
     let regex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,5}/g;
     if (!regex.test(e.target.value)) {
         inputEmail.classList.add('form-error');
@@ -333,9 +390,17 @@ inputEmail.addEventListener('keyup', (e) => {
 });
 
 /*Form Validation for Mobile number */
+
+//Refernce to the input field
 var inputMobile = document.getElementById('form-mobile');
+
+//Reference to the element that shows the error
 var inputMobileErrorMessage = document.getElementById('form-mobile-error');
+
+//Eventlistener Keyup to validate input on each key up
 inputMobile.addEventListener('keyup', (e) => {
+
+    //Regular Expression
     let regex = /^\+91[ -]?[\d]{10}$/g;
     if (!regex.test(e.target.value)) {
         inputMobile.classList.add('form-error');
@@ -349,9 +414,17 @@ inputMobile.addEventListener('keyup', (e) => {
 
 
 /*Form validation for Website*/
+
+//Refernce to the input field
 var inputWebsite = document.getElementById('form-website');
+
+//Reference to the element that shows the error
 var inputWebsiteErrorMessage = document.getElementById('form-website-error');
+
+//Eventlistener Keyup to validate input on each key up
 inputWebsite.addEventListener('keyup', (e) => {
+
+    //Regular Expression
     let regex = /^(https|http):\/\/[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z]+$/g;
     if (!regex.test(e.target.value)) {
         inputWebsite.classList.add('form-error');
